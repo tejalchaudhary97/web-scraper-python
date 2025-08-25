d1ec560485a9125f905c3a6ca9583626cbd6d03b
Tejal Chaudhary <tejalchaudhary97@gmail.com>
2025-08-25 12:05:20 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,17 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-URL = "https://news.ycombinator.com/"
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, "html.parser")
+def scrape_news():
+    URL = "https://news.ycombinator.com/"
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-titles = []
-links = []
+    titles = []
+    links = []
 
-for item in soup.select(".titleline a"):
-    titles.append(item.text)
-    links.append(item.get("href"))
+    for item in soup.select(".titleline a"):
+        titles.append(item.text)
+        links.append(item.get("href"))
 
-data = pd.DataFrame({"Title": titles, "Link": links})
-data.to_csv("news.csv", index=False)
-print("Scraping completed. Check news.csv")
+    data = pd.DataFrame({"Title": titles, "Link": links})
+    return data
